7afbe0da20e3523d32a92e3cce9ba2b5d489e524
Jez Blackmore <jezblackmore@Jezs-MacBook-Air.local>
2022-01-25 15:48:37 +0000

    updated house list page

--- propertydating/src/components/pages/likedHouses/FavouriteCard.js
+++ propertydating/src/components/pages/likedHouses/FavouriteCard.js
@@ -1,10 +1,18 @@
 import ChatIcon from "./ChatIcon";
 import BinIcon from "./BinIcon";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function FavouriteCard({ house, index }) {
-  console.log(house);
-  return (
-    <div className="favourite-card">
+  
+  let navigate = useNavigate();
+  
+  const handleClick = (e) => {
+    e.stopPropagation();
+    navigate(`/house-profile`);
+  }
+
+  return (  
+    <div className="favourite-card" onClick={handleClick}>
       <div className="favourite-card__image">
         <img src={house.house_images[0].key} alt="house pic"></img>
         <div className="favourite-card__image__chat-icon">
@@ -17,12 +25,13 @@ export default function FavouriteCard({ house, index }) {
         </div>
       </div>
       <div className="favourite_card__meta">
-        <h2>{`${house.postcode} (with address)`}</h2>
+        <h2>Postcode: {`${house.postcode} (with address)`}</h2>
         <ul>
-          <li>{house.price}</li>
-          <li>{house.type}</li>
+          <li>Price: £{house.price}</li>
+          <li>Type: {house.type}</li>
         </ul>
       </div>
     </div>
+   
   );
 }
