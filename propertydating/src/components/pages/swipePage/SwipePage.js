62b7c5a6a6ac5f9265f22167522c4496db152892
Darren Evans <seefer@outlook.com>
2022-01-26 14:19:40 +0000

    added Tailwind fix, adds form controls to AddProperty.js

--- propertydating/src/components/pages/swipePage/SwipePage.js
+++ propertydating/src/components/pages/swipePage/SwipePage.js
@@ -20,7 +20,7 @@ export default function HomePage() {
   }, [houseIndex, testHouses]);
 
   useEffect(() => {
-    fetchProperties().then((res) => {
+    fetchProperties().then(res => {
       setTestHouses(res);
       setAmountOfProperties(res.length);
     });
