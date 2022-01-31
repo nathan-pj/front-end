8d481bfd3fdc26b95cf450a832f858a069da71b9
Darren Evans <seefer@outlook.com>
2022-01-31 15:29:52 +0000

    Adds destructuring, isLoading and isError patterns. Put house image index into state ready to work on changing image display to corousel with controls

--- propertydating/src/components/pages/houseProfile/HouseProfile.js
+++ propertydating/src/components/pages/houseProfile/HouseProfile.js
@@ -3,20 +3,33 @@ import { useParams } from "react-router-dom";
 import { fetchPropertyById } from "../../../utils/api";
 import { currencyFormatter } from "../../../utils/currencyFormatter";
 
-//TODO Remove this static placeholder house pic for <img> tag for dynamic image
-import placeHolderPic from "./housepic.jpg";
+//TODO Remove this static placeholder house pic for <img> tag for dynamic image.
+// Would we need it for situations where images are not uploaded yet?
+import placeHolderPic from "./housePlaceholderPic.jpg";
 
 export default function HouseProfile() {
   const { house_id } = useParams();
 
-  console.log(house_id);
   const [property, setProperty] = useState({});
+  const { price, postcode, house_images, offer_made, property_type, beds } =
+    property;
+
+  const [imageIndex, setImageIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetchPropertyById(house_id).then(res => {
-      setProperty(res);
-    });
-  }, [house_id]);
+    setIsLoading(true);
+    fetchPropertyById(house_id)
+      .then(res => {
+        setProperty(res);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setIsError(true);
+        console.log(err);
+      });
+  }, [house_id, imageIndex]);
 
   return (
     <div className="container">
@@ -24,19 +37,17 @@ export default function HouseProfile() {
       <div className="bg-gray-200 rounded shadow-md ">
         <img
           className="w-full rounded-b-none rounded-t-md "
-          src={property.house_images ? property.house_images[0] : null}
+          src={house_images ? house_images[imageIndex] : null}
           alt="house profile"
         ></img>
 
         <div className="p-2">
           <ul>
-            <li>Price: {currencyFormatter.format(property.price)}</li>
-            <li>Postcode: {property.postcode}</li>
-            <li>Property Type: {property.property_type}</li>
-            <li>Bedrooms: {property.beds}</li>
-            <li>
-              Offer made on property? {property.offer_made ? "YES" : null}
-            </li>
+            <li>Price: {currencyFormatter.format(price)}</li>
+            <li>Postcode: {postcode}</li>
+            <li>Property Type: {property_type}</li>
+            <li>Bedrooms: {beds}</li>
+            <li>Offer made on property? {offer_made ? "Yes." : "No."}</li>
           </ul>
         </div>
       </div>
--- propertydating/src/utils/currencyFormatter.js
+++ propertydating/src/utils/currencyFormatter.js
@@ -7,4 +7,10 @@ export const currencyFormatter = new Intl.NumberFormat("en-UK", {
   // Following options are needed to round to whole numbers if required.
   //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as £2,500.1)
   //maximumFractionDigits: 0, // (causes 2500.99 to be printed as £2,501)
+
+  // Function call looks like this:currencyFormatter.format(price)
+
+  // currencyFormatter.format(price)
+
+  // See houseProfile component for example of use there
 });
