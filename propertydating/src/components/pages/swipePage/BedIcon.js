f3b59e486209f8eb2a99b3e2ffb8054610be6f70
nathan-pj <nathan.pagejones@gmail.com>
2022-02-02 14:42:10 +0000

    polish css

---
+++ propertydating/src/components/pages/swipePage/BedIcon.js
@@ -0,0 +1,20 @@
+export default function BedIcon() {
+  return (
+    <svg
+      class="h-8 w-8 text-red-500"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      {" "}
+      <path stroke="none" d="M0 0h24v24H0z" />{" "}
+      <path d="M3 7v11m0 -4h18m0 4v-8a2 2 0 0 0 -2 -2h-8v6" />{" "}
+      <circle cx="7" cy="10" r="1" />
+    </svg>
+  );
+}
--- propertydating/src/components/pages/swipePage/ImageArrowRight.js
+++ propertydating/src/components/pages/swipePage/ImageArrowRight.js
@@ -16,15 +16,17 @@ export default function ImageArrowRight({
       {currentImage < numOfImages ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="image-arrow-right"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+          class="image-arrow-right"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
           onClick={handleClick}
         >
           <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-            clipRule="evenodd"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
           />
         </svg>
       ) : null}
